1e7ccf8cdcf69ae5e9b153ff901eddb4309ceb34
khan-atiq <atiq.khan2288@gmail.com>
2023-03-27 04:29:38 +0530

    secured

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIATBVH25SRNRLXLYVG"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
